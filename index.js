4cfe5e3e1bf3e371caee50d28338bce8d2a681ff
Felipe SCHULZ <56260943+schulzf@users.noreply.github.com>
2021-10-28 15:45:42 +0200

    Candidate Card Swipe Animation
    User feedback now possible with swipe gestures
    
    TODOs:
    -add user profile picture
    -add like to user profile

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
  * @format
  */
-import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
+import 'react-native-gesture-handler';
 import {name as appName} from './app.json';
 import Navigation from './Navigation';
 
--- src/components/CandidateCard.tsx
+++ src/components/CandidateCard.tsx
@@ -1,20 +1,91 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import Animated, {
+  runOnJS,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import {globalStyles} from '../utils/globalStyles';
 
 interface CandidateCardProps {}
 
 const CandidateCard: React.FC<CandidateCardProps> = () => {
+  const [sentimentColor, setSentimentColor] = useState<
+    '#FFF' | '#5abb8850' | '#de460a50'
+  >('#FFF');
+
+  const {width: screen_width} = Dimensions.get('window');
+
+  const translateX = useSharedValue(0);
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+      {
+        rotateZ: `${translateX.value / 15}deg`,
+      },
+      {
+        scale: 1 - Math.abs(translateX.value) / 1000,
+      },
+    ],
+    opacity: 1 - Math.abs(translateX.value) / 500,
+  }));
+
+  const like_threshold = screen_width / 2.4;
+  const dislike_threshold = (screen_width / 2.4) * -1;
+
+  const panGesture = useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
+    onActive: event => {
+      translateX.value = event.translationX;
+      if (
+        event.translationX > dislike_threshold &&
+        event.translationX < like_threshold
+      ) {
+        runOnJS(setSentimentColor)('#FFF');
+      }
+      if (event.translationX < dislike_threshold) {
+        runOnJS(setSentimentColor)('#de460a50');
+      }
+      if (event.translationX > like_threshold) {
+        runOnJS(setSentimentColor)('#5abb8850');
+      }
+    },
+    onEnd: event => {
+      if (event.translationX < dislike_threshold) {
+        console.warn('DISLIKE');
+      }
+      if (event.translationX > like_threshold) {
+        console.warn('LIKE');
+      }
+      translateX.value = withTiming(0);
+      runOnJS(setSentimentColor)('#FFF');
+    },
+  });
+
+  useEffect(() => {
+    console.log(translateX.value);
+  }, [translateX.value]);
+
   return (
-    <View style={styles.wrapper}>
-      <View style={styles.card_container}>
-        <View style={styles.card_photo}></View>
-        <View style={[globalStyles.mt2, globalStyles.p1]}>
-          <Text style={styles.text_candidate_name}>Name of Candidate</Text>
-          <Text style={styles.text_candidate_age}>26</Text>
+    <PanGestureHandler onGestureEvent={panGesture}>
+      <Animated.View style={[styles.wrapper, cardStyle]}>
+        <View
+          style={[styles.card_container, {backgroundColor: sentimentColor}]}>
+          <View style={styles.card_photo}></View>
+          <View style={[globalStyles.mt2, globalStyles.p1]}>
+            <Text style={styles.text_candidate_name}>Name of Candidate</Text>
+            <Text style={styles.text_candidate_age}>26</Text>
+          </View>
         </View>
-      </View>
-    </View>
+      </Animated.View>
+    </PanGestureHandler>
   );
 };
 
@@ -29,7 +100,6 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     padding: 10,
-    backgroundColor: '#FFFFFF',
     borderRadius: 15,
     shadowColor: '#000',
     shadowOffset: {
